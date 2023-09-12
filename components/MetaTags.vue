fefd22dd6589a743ff91882c6af838e7f665980d
Akayy <techakayy@gmail.com>
2023-09-12 19:47:35 +1000

    Updated meta for non-blocking css

--- components/MetaTags.vue
+++ components/MetaTags.vue
@@ -3,6 +3,11 @@
   import type { Script } from '@unhead/schema'
   type TurboScript = Script & { once: true }
 
+  const fonts =
+    'https://fonts.googleapis.com/css?family=DM+Sans:400,500,700|Inter:100,200,300,400,500,600,700,800,900&display=swap'
+  const googleapis = 'https://fonts.googleapis.com'
+  const gstatic = 'https://fonts.gstatic.com'
+
   const config = useRuntimeConfig()
   const { title, description } = config.public
   const route = useRoute()
@@ -27,6 +32,22 @@
       { property: 'author', content: 'Pinegrow' },
     ],
     script: [{ children: checkDarkTheme, once: true } as TurboScript],
+    // prefetch fira font
+    link: [
+      { rel: 'dns-prefetch', href: googleapis },
+      { rel: 'dns-prefetch', href: gstatic },
+      { rel: 'preconnect', crossorigin: 'anonymous', href: googleapis },
+      { rel: 'preconnect', crossorigin: 'anonymous', href: gstatic },
+      {
+        rel: 'preload',
+        as: 'style',
+        onload: "this.onload=null;this.rel='stylesheet'",
+        href: fonts,
+      },
+    ],
+    noscript: [
+      `<link rel="stylesheet" crossorigin="anonymous" href="${fonts}" />`,
+    ],
   })
 </script>
 
