f832c7b06b6b8a9c844afdc5c6bb39572094da28
Akayy <techakayy@gmail.com>
2023-09-14 18:08:14 +1000

    Blank template

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,17 @@
 <template>
-  <v-card class="mx-auto">
-    <div>
-      <MetaTags />
-      <div
-        class="flex flex-col bg-white dark:bg-neutral-950 text-neutral-900 dark:text-neutral-50"
-      >
-        <NavBar class="fixed bg-white dark:bg-neutral-950 shadow z-10" />
-        <div class="mt-24 shadow">
-          <slot />
-        </div>
-        <TheFooter />
-      </div>
+  <!-- <v-card class="mx-auto"> -->
+  <div>
+    <MetaTags />
+    <div
+      class="flex flex-col bg-white dark:bg-neutral-950 text-neutral-900 dark:text-neutral-50"
+    >
+      <!-- <NavBar class="fixed bg-white dark:bg-neutral-950 shadow z-10" /> -->
+      <!-- <div class="mt-24 shadow"> -->
+      <slot />
+      <!-- </div> -->
+      <!-- <TheFooter /> -->
     </div>
-  </v-card>
+  </div>
+  <!-- </v-card> -->
 </template>
 <style></style>
