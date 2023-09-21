9809e320e7033d9ec130e111ea26aad1ac59c876
Akayy <techakayy@gmail.com>
2023-09-22 04:07:59 +1000

    Fixed hamburger & image optimizations

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,11 +1,10 @@
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.hook('vuetify:before-create', ({ isDev, vuetifyOptions }) => {
-    if (process.client && isDev) {
-      // eslint-disable-next-line no-console
-      console.log('vuetify:plugin:hook', vuetifyOptions)
-    }
-  })
-
+  // nuxtApp.hook('vuetify:before-create', ({ isDev, vuetifyOptions }) => {
+  //   if (process.client && isDev) {
+  //     // eslint-disable-next-line no-console
+  //     console.log('vuetify:plugin:hook', vuetifyOptions)
+  //   }
+  // })
   // nuxtApp.hook(
   //   'vuetify:ssr-client-hints',
   //   ({ vuetifyOptions, ssrClientHints, ssrClientHintsConfiguration }) => {
