d762e18cd3b2070c5cbfa97786ab3c21c3ac510a
Akayy <techakayy@gmail.com>
2023-11-13 21:20:02 +1100

    Meta updates

--- pages/store/[...id].vue
+++ pages/store/[...id].vue
@@ -1,4 +1,17 @@
 <script setup lang="ts">
+  definePageMeta({
+    // layout: 'default',
+    name: 'store',
+    // alias: '[...slug]',
+    title: 'Store',
+    description: `Shop 'til You Drop: Your E-Commerce Haven!`,
+    navOrder: '3',
+    hidden: true,
+    type: 'primary',
+    icon: 'i-mdi-home',
+    // ogImage: 'images/ogImage.png', // url or local images inside public folder, for eg, ~/public/images/ogImage.png
+  })
+
   const route = useRoute()
   const { id: productId } = route.params
 
