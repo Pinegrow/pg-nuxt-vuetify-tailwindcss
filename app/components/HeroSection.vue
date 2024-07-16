c2f85040f3a1ff057e2ed58bf0f1ae9fdaf517f9
Akayy <techakayy@gmail.com>
2024-07-17 01:47:00 +1000

    Added BackgroundImageWrapper for HeroSection

--- app/components/HeroSection.vue
+++ app/components/HeroSection.vue
@@ -1,38 +1,14 @@
 <script setup lang="ts">
   import { heroImageUrl } from '@/utils/hero'
 
-  const { optimizeImage } = useOptimizeImage()
-  const heroImageOptimized = {
-    alt: `hero`,
-    cover: true,
-    ...optimizeImage(
-      heroImageUrl,
-      /* options */
-      {
-        /* If using local images instead of unsplash url, enable netlify provider */
-        // provider:
-        //     process.env.NODE_ENV === 'production'
-        //       ? 'netlify'
-        //       : null /* defaults to ipx or ipxStatic */,
-        placeholder: false, // placeholder image before the actual image is fully loaded.
-      },
-      true /* return bgStyles */,
-    ),
-  }
-
-  const heroImage = heroImageOptimized.src
-  const bgStyles = heroImageOptimized.bgStyles
+  const heroCaption = 'Welcome To'
+  const heroTitle = 'Vue Designer'
+  const heroDescription =
+    'A desktop drag-and-drop editor for Mac, Windows and Linux that lets you visually design your Vue apps.'
 </script>
 <template>
   <section>
-    <v-img
-      :src="heroImageOptimized.src"
-      :srcset="heroImageOptimized.srcset"
-      :sizes="heroImageOptimized.sizes"
-      :max-height="800"
-      cover
-    >
-      <!-- <v-img :src="heroImageUrl" :max-height="800" cover> -->
+    <BackgroundImageWrapper :image-url="heroImageUrl">
       <div class="container flex flex-col h-full justify-end mx-auto">
         <div class="flex flex-col items-end pb-8 px-4 w-full md:pb-20">
           <h2
@@ -49,7 +25,7 @@
           />
         </div>
       </div>
-    </v-img>
+    </BackgroundImageWrapper>
   </section>
 </template>
 <style scoped></style>
